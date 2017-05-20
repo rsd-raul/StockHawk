824e9d4d3893de7c448e39caceb9305f73c2bcbf
rsd-raul <rsd_raul@yahoo.es>
2017-05-21 01:55:23 +0200

    LinearGraph upgrade. Scrollable stock graph (group by month).

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -7,11 +7,14 @@ import com.udacity.stockhawk.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private String[] mLabels = {"Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep", "Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep", "Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep"};
+    private float[] mValues = {900.5f, 920.4f, 890.3f, 960f, 975.2f, 990.2f, 950f, 930f, 950.9f, 900.5f, 920.4f, 890.3f, 960f, 975.2f, 990.2f, 950f, 930f, 950.9f, 900.5f, 920.4f, 890.3f, 960f, 975.2f, 990.2f, 950f, 930f, 950.9f};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        new LinearGraph(this).init();
+        new LinearGraph(this).show(mLabels, mValues);
     }
 }
--- app/src/main/java/com/udacity/stockhawk/ui/LinearGraph.java
+++ app/src/main/java/com/udacity/stockhawk/ui/LinearGraph.java
@@ -2,10 +2,13 @@ package com.udacity.stockhawk.ui;
 
 import android.animation.PropertyValuesHolder;
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import com.db.chart.Tools;
 import com.db.chart.animation.Animation;
 import com.db.chart.model.LineSet;
@@ -15,117 +18,142 @@ import com.db.chart.view.LineChartView;
 import com.udacity.stockhawk.R;
 
 
-public class LinearGraph extends CardController {
+public class LinearGraph {
 
 	private final LineChartView mChart;
-	private final Context mContext;
+	private final Activity mActivity;
 
-	private final String[] mLabels = {"Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep"};
-	private final float[] mValues = {900.5f, 920.4f, 890.3f, 960f, 975.2f, 990.2f, 950f, 930f, 950.9f};
-    private int mMaxValue = 1000;
-    private int mMinValue = 880;
-    private int selected = mValues.length-1;
+	private String[] mLabels;
+	private float[] mValues;
+    private int mMaxValue, mMinValue, mSelected, mValuesPerWindow;
 
 	private Tooltip mTip;
 
-	private Runnable mBaseAction;
+	public LinearGraph(Activity activity) {
 
-	public LinearGraph(Activity context) {
-		super(context);
-
-		mContext = context;
-		mChart = (LineChartView) context.findViewById(R.id.chart1);
+		mActivity = activity;
+		mChart = (LineChartView) activity.findViewById(R.id.chart1);
 	}
 
-	@Override
-	public void show(Runnable action) {
-		super.show(action);
+	public void show(String[] labels, float[] values) {
+        mLabels = labels;
+        mValues = values;
 
-		// Tooltip
-		mTip = new Tooltip(mContext, R.layout.linechart_three_tooltip, R.id.value);
-
-//		((TextView) mTip.findViewById(R.id.value)).setTypeface(
-//                Typeface.createFromAsset(mContext.getAssets(), "OpenSans-Semibold.ttf"));
-
-		mTip.setVerticalAlignment(Tooltip.Alignment.BOTTOM_TOP);
-		mTip.setDimensions((int) Tools.fromDpToPx(65), (int) Tools.fromDpToPx(25));
-
-		mTip.setEnterAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 1),
-			    PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f),
-			    PropertyValuesHolder.ofFloat(View.SCALE_X, 1f)).setDuration(200);
-
-		mTip.setExitAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 0),
-			    PropertyValuesHolder.ofFloat(View.SCALE_Y, 0f),
-			    PropertyValuesHolder.ofFloat(View.SCALE_X, 0f)).setDuration(200);
-
-		mTip.setPivotX(Tools.fromDpToPx(65) / 2);
-		mTip.setPivotY(Tools.fromDpToPx(25));
-
-		mChart.setTooltips(mTip);
-
-		// Data
-		LineSet dataset = new LineSet(mLabels, mValues);
-		dataset.setColor(Color.parseColor("#758cbb"))
-				  .setFill(Color.parseColor("#2d374c"))
-				  .setDotsColor(Color.parseColor("#758cbb"))
-				  .setThickness(4)
-				  .setDashed(new float[] {10f, 10f})
-				  .beginAt(mValues.length-2);
-		mChart.addData(dataset);
-
-		dataset = new LineSet(mLabels, mValues);
-		dataset.setColor(Color.parseColor("#b3b5bb"))
-				  .setFill(Color.parseColor("#2d374c"))
-				  .setDotsColor(Color.parseColor("#ffc755"))
-				  .setThickness(4)
-				  .endAt(mValues.length-1);
-		mChart.addData(dataset);
-
-		// Chart
-		mChart.setBorderSpacing(Tools.fromDpToPx(15))
-				  .setAxisBorderValues(mMinValue, mMaxValue)
-				  .setYLabels(AxisRenderer.LabelPosition.NONE)
-				  .setLabelsColor(Color.parseColor("#6a84c3"))
-				  .setXAxis(false)
-				  .setYAxis(false);
-
-		mBaseAction = action;
-		Runnable chartAction = new Runnable() {
-			@Override
-			public void run() {
-				mBaseAction.run();
-				mTip.prepare(mChart.getEntriesArea(0).get(selected), mValues[selected]);
-				mChart.showTooltip(mTip, true);
-			}
-		};
-
-		Animation anim = new Animation().setEasing(new BounceInterpolator()).setEndAction(chartAction);
-
-		mChart.show(anim);
+        calculateGraphParameters();
+        configureGraph();
+        configureTooltip();
+        initializeAndShowGraph();
 	}
 
-	@Override
-	public void update() {
-		super.update();
+    private void calculateGraphParameters() {
+        mSelected = mValues.length-1;
+        mValuesPerWindow = 10;
 
-		mChart.dismissAllTooltips();
-		if (firstStage) {
-			mChart.updateValues(0, mValues);
-			mChart.updateValues(1, mValues);
-		} else {
-			mChart.updateValues(0, mValues);
-			mChart.updateValues(1, mValues);
-		}
-		mChart.getChartAnimation().setEndAction(mBaseAction);
-		mChart.notifyDataUpdate();
-	}
+        int minValue = Integer.MAX_VALUE, maxValue = Integer.MIN_VALUE;
 
-	@Override
-	public void dismiss(Runnable action) {
+        for (float value : mValues) {
+            if(value > maxValue)
+                maxValue = Math.round(value);
+            if(value < minValue)
+                minValue = Math.round(value);
+        }
 
-		super.dismiss(action);
+        int difference = maxValue - minValue;
+        difference /= 10;
 
-		mChart.dismissAllTooltips();
-		mChart.dismiss(new Animation().setEasing(new BounceInterpolator()).setEndAction(action));
-	}
+        // Adjust min and max so they are not exactly the borders of the graph
+        mMaxValue = maxValue + difference;
+        mMinValue = minValue - difference;
+    }
+
+    private void configureGraph() {
+        // Get the metrics to calculate the Graph size
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        mActivity.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int deviceWidth = displaymetrics.widthPixels;
+        int itemsBasedWidth = (mValues.length * deviceWidth / mValuesPerWindow);
+
+        // If the items have enough space, return
+        if (deviceWidth >= itemsBasedWidth)
+            return;
+
+        // If the items need more spacing, dynamically increase the graph size
+        mChart.setLayoutParams(new LinearLayout.LayoutParams(itemsBasedWidth, mChart.getLayoutParams().height));
+
+        // Scroll the view to the end
+        final HorizontalScrollView hsv = (HorizontalScrollView) mActivity.findViewById(R.id.sv_chart1);
+        hsv.setHorizontalScrollBarEnabled(false);
+        hsv.postDelayed(new Runnable() {
+            public void run() {
+                hsv.setSmoothScrollingEnabled(false);
+                hsv.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+            }
+        }, 50L);
+    }
+
+    private void configureTooltip() {
+        float widthPx = Tools.fromDpToPx(65), heightPx = Tools.fromDpToPx(25);
+
+        mTip = new Tooltip(mActivity, R.layout.linechart_three_tooltip, R.id.value);
+
+        mTip.setVerticalAlignment(Tooltip.Alignment.BOTTOM_TOP);
+        mTip.setDimensions((int) widthPx, (int) heightPx);
+
+        mTip.setEnterAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 1),
+                PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f),
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 1f)).setDuration(200);
+        mTip.setExitAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 0),
+                PropertyValuesHolder.ofFloat(View.SCALE_Y, 0f),
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 0f)).setDuration(200);
+
+        mTip.setPivotX(widthPx / 2);
+        mTip.setPivotY(heightPx);
+    }
+
+    private void initializeAndShowGraph() {
+        mChart.setTooltips(mTip);
+
+        // Data
+        mChart.addData(getFixedValues());
+        mChart.addData(getDynamicValues());
+
+        // Chart
+        mChart.setBorderSpacing(Tools.fromDpToPx(15))
+                .setAxisBorderValues(mMinValue, mMaxValue)
+                .setYLabels(AxisRenderer.LabelPosition.NONE)
+                .setLabelsColor(Color.parseColor("#6a84c3"))
+                .setXAxis(false)
+                .setYAxis(false);
+
+        Runnable chartAction = new Runnable() {
+            @Override
+            public void run() {
+                mTip.prepare(mChart.getEntriesArea(0).get(mSelected), mValues[mSelected]);
+                mChart.showTooltip(mTip, true);
+            }
+        };
+
+        Animation anim = new Animation().setEasing(new BounceInterpolator()).setEndAction(chartAction);
+
+        mChart.show(anim);
+    }
+
+    private LineSet getFixedValues() {
+        return new LineSet(mLabels, mValues)
+                .setColor(ContextCompat.getColor(mActivity, R.color.graph_fixed_line))
+                .setFill(ContextCompat.getColor(mActivity, R.color.graph_fill))
+                .setDotsColor(ContextCompat.getColor(mActivity, R.color.graph_fixed_dot))
+                .setThickness(4)
+                .endAt(mValues.length-1);
+    }
+
+    private LineSet getDynamicValues() {
+        return new LineSet(mLabels, mValues)
+                .setColor(ContextCompat.getColor(mActivity, R.color.graph_dynamic_dot_line))
+                .setFill(ContextCompat.getColor(mActivity, R.color.graph_fill))
+                .setDotsColor(ContextCompat.getColor(mActivity, R.color.graph_dynamic_dot_line))
+                .setThickness(4)
+                .setDashed(new float[] {10f, 10f})
+                .beginAt(mValues.length-2);
+    }
 }
